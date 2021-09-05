f0e42366af4479e8780a49219031283f0236dde3
jkkCoder <jayeshk118@gmail.com>
2021-09-05 23:07:14 +0530

    fixed error(spelling mistake in forecast data)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("unable to find location")
         }
         else{
-            callback(undefined,`${body.current.weather_description[0]}. It is currently ${body.current.temperature} deg celsius. And it feels like ${body.current.feelslike}`)
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} deg celsius. And it feels like ${body.current.feelslike}`)
         }
     })
 
